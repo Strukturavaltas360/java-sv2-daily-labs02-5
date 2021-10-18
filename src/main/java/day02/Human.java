b896cac52e665abb816f1741d27611de7d74f081
ht0m <hartht@gmail.com>
2021-10-18 15:48:29 +0200

    Human class created

---
+++ src/main/java/day02/Human.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Human {
+
+    private Sting name;
+    provate int age;
+
+    public Human(Sting name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Sting getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
