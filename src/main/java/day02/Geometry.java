6b9b637b19d84fd972c66ae458150774328b61e2
ht0m <hartht@gmail.com>
2021-10-19 16:57:55 +0200

    day02 task

---
+++ src/main/java/day02/Geometry.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+
+        double a = 10;
+        double b = 5;
+
+        Rectangle rectangle = new Rectangle(a,b);
+
+        System.out.println(rectangle.calculateArea());
+
+    }
+
+}
