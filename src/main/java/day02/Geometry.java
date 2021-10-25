bfa7888fe293c2e51f18f27c01555ca3cb00fd73
ht0m <hartht@gmail.com>
2021-10-25 14:39:11 +0200

    Day02 Geometry/Rectangle area

--- src/main/java/day02/Geometry.java
+++ src/main/java/day02/Geometry.java
@@ -6,10 +6,13 @@ public class Geometry {
         double a = 10;
         double b = 5;
 
-        Rectangle rectangle = new Rectangle(a,b);
+        Rectangle rectangle1 = new Rectangle(a,b);
+        Rectangle rectangle2 = new Rectangle(3,7);
+        Rectangle rectangle3 = new Rectangle(5.4,8.2);
 
-        System.out.println(rectangle.calculateArea());
+        System.out.println(rectangle1.calculateArea());
+        System.out.println(rectangle2.calculateArea());
+        System.out.println(rectangle3.calculateArea());
 
     }
-
 }
